59a632ec381fee6ea1bf6f891b47a619ad0f9f4e
jharnath <88421464+jharnath@users.noreply.github.com>
2024-02-13 17:35:35 +1100

    active classnames

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./links.module.css"
+import NavLinks from "./navLinks/navLinks";
 
 const Links = () => {
 
@@ -21,11 +22,25 @@ const Links = () => {
             path: "/blog",
         }
     ];
+
+    const session = true;
+    const isAdmin = true;
+
+
     return (
         <div className={styles.links}>
         {links.map((link)=>(
             <NavLinks item={link} key={link.title}/>
-        ))}
+        ))}{
+            session ? (
+                <>
+                    {isAdmin && (<NavLinks item={{title:"Admin", path: "/admin"}}/>)}
+                    <button className={styles.logout}>Logout</button>
+                </>
+            ) : (
+                <NavLinks item={{title:"Login", path: "/login"}}/>
+            )
+        }
         </div>
     );
 }
--- src/components/navbar/links/navLinks/navLinks.jsx
+++ src/components/navbar/links/navLinks/navLinks.jsx
@@ -1,9 +1,15 @@
+"use client"
+
 import Link from "next/link"
 import styles from "./navLinks.module.css"
+import { usePathname } from "next/navigation"
 
 const NavLinks = ({item}) => {
+
+    const pathName = usePathname();
+
     return(
-        <Link href={item.path} className={`${styles.container}`}>
+        <Link href={item.path} className={`${styles.container} ${pathName === item.path && styles.active}`}>
             {item.title}</Link>
     )
 }
