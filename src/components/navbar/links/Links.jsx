09ce500ca1cd82ca136e9c67d9d5d352de303072
Jharnath <jharnath@gmail.com>
2024-02-25 22:13:42 +1100

    responsive navlink changes

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,9 +1,11 @@
-import Link from "next/link";
+"use client"
+
+import { useState } from "react";
 import styles from "./links.module.css"
 import NavLinks from "./navLinks/navLinks";
 
 const Links = () => {
-
+    
     const links = [
         {
             title: "Home",
@@ -23,26 +25,38 @@ const Links = () => {
         }
     ];
 
+    const [open,setOpen] = useState(false)
+    //temp
     const session = true;
     const isAdmin = true;
 
 
     return (
-        <div className={styles.links}>
-        {links.map((link)=>(
-            <NavLinks item={link} key={link.title}/>
-        ))}{
-            session ? (
-                <>
-                    {isAdmin && (<NavLinks item={{title:"Admin", path: "/admin"}}/>)}
-                    <button className={styles.logout}>Logout</button>
-                </>
-            ) : (
-                <NavLinks item={{title:"Login", path: "/login"}}/>
-            )
-        }
+        <div className={styles.container}>
+            <div className={styles.links}>
+            {links.map((link)=>(
+                <NavLinks item={link} key={link.title}/>
+            ))}{
+                session ? (
+                    <>
+                        {isAdmin && (<NavLinks item={{title:"Admin", path: "/admin"}}/>)}
+                        <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLinks item={{title:"Login", path: "/login"}}/>
+                )
+            }
+            </div>
+            <button className={styles.menuButton} onClick={()=>setOpen((prev) => !prev)}>Menu</button>
+            {
+                open && <div className={styles.mobileLinks}>
+                    {links.map((link) =>(
+                        <NavLinks item={link} key={link.title}/>
+                    ))}
+                </div>
+            }
         </div>
     );
-}
+};
 
 export default Links
