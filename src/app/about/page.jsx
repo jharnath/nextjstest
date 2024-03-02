8c20c99e3400c323c53270e36833a22ea47a6b65
Jharnath <jharnath@gmail.com>
2024-03-02 22:21:42 +1100

    footer & about - 1:19

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,8 +3,33 @@ import styles from "./about.module.css"
 
 const AboutPage = () => {
     return(
-        <div className={styles.imgContainer}>
-            <Image src="/about.png" alt="about" fill/> 
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Site</h2>
+                <h1 className={styles.title}>
+                    This is a NextJs Fullstack test site
+                </h1>
+                <p className={styles.desc}>
+                    Some more stuff about this project idk, this project has a home, about, contact and blog page.
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h1>10K +</h1>
+                        <p>Years of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>234K +</h1>
+                        <p>People reached</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>5K +</h1>
+                        <p>Services and plugins</p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image src="/about.png" alt="about" fill/> 
+            </div>
         </div>
     )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,15 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
     return(
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                Jharnath
+            </div>
+            <div className={styles.text}>
+                Nextjs Test Site @ All rights reserved.
+            </div>
+        </div>
     )
 }
 
